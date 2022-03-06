4b6e552db38196dc2d033d3607ee49d6d7826356
Guanghui Wang <guanghui-wang@foxmail.com>
2022-03-06 16:42:08 +0800

    style: apploading

--- src/components/common/AppLoading.tsx
+++ src/components/common/AppLoading.tsx
@@ -1,9 +1,9 @@
 import {CdsProgressCircle} from '@cds/react/progress-circle';
 
-export default function AppLoading({text}: {text?: string}) {
+export default function AppLoading({text, size = 'xxl'}: {text?: string; size?: string}) {
   return (
-    <div className="flex h-full items-center justify-center gap-4 p-4">
-      <CdsProgressCircle size="xxl" status="info"></CdsProgressCircle>
+    <div className="flex h-full items-center justify-center gap-4 p-4" cds-text="body">
+      <CdsProgressCircle size={size} status="info"></CdsProgressCircle>
       Loading {text} ...
     </div>
   );
