b2b1c1520fca863d4243b5168af509e1c6612b0a
Guanghui Wang <guanghui-wang@foxmail.com>
2022-03-08 11:30:22 +0800

    refactor: add id name for multicheckbox

--- src/components/common/MultiCheckbox.tsx
+++ src/components/common/MultiCheckbox.tsx
@@ -2,34 +2,39 @@ import {CdsCheckbox, CdsCheckboxGroup} from '@cds/react/checkbox';
 import {CdsControlMessage} from '@cds/react/forms';
 import {useState} from 'react';
 
+interface CheckboxItem {
+  name: string;
+  id: string; // used for value
+}
+
 export default function MultiCheckbox({
   label,
   inputName,
-  defaultCheckedValues,
-  allValues,
+  defaultCheckedItems,
+  allItems,
   description,
   selectChange,
 }: {
   label: string;
   inputName: string;
-  defaultCheckedValues: string[];
-  allValues: string[];
+  defaultCheckedItems: CheckboxItem[];
+  allItems: CheckboxItem[];
   description?: string;
   selectChange?: (items: string[]) => void;
 }) {
-  const [selectedValues, setSelectedValues] = useState([...defaultCheckedValues]);
+  const [selectedValues, setSelectedValues] = useState(() => defaultCheckedItems.map(i => i.id));
 
   return (
     <CdsCheckboxGroup>
       <label>{label}</label>
-      {allValues.map(item => (
-        <CdsCheckbox key={item}>
-          <label>{item}</label>
+      {allItems.map(item => (
+        <CdsCheckbox key={item.id}>
+          <label>{item.name}</label>
           <input
             type="checkbox"
             name={inputName}
-            value={item}
-            checked={selectedValues.includes(item)}
+            value={item.id}
+            checked={selectedValues.includes(item.id)}
             onChange={e => {
               const value = e.target.value;
               if (e.target.checked) {
