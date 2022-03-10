fa023e2e999ac58fb2267c8b41e253642f66c90e
Guanghui Wang <guanghui-wang@foxmail.com>
2022-03-10 19:52:55 +0800

    style: update appCard

--- src/components/common/AppCard.tsx
+++ src/components/common/AppCard.tsx
@@ -1,7 +1,17 @@
-export default function AppCard({checked, children}: {checked: boolean; children: React.ReactNode}) {
+export default function AppCard({
+  checked,
+  children,
+  className,
+}: {
+  checked: boolean;
+  children: React.ReactNode;
+  className?: string;
+}) {
   return (
     <div
-      className={`border-2 bg-white p-8 shadow dark:bg-gray-800 ${checked ? 'border-green-500' : 'border-transparent'}`}
+      className={`${className} border-2 bg-white p-8 shadow dark:bg-gray-800 ${
+        checked ? 'border-green-500' : 'border-transparent'
+      }`}
     >
       {children}
     </div>
