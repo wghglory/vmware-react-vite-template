ce484b08dd8e59462714a7a46978e67ece9e7c9b
Guanghui Wang <guanghui-wang@foxmail.com>
2022-03-11 16:58:20 +0800

    feat: add classname for pageContainer

--- src/components/common/PageContainer.tsx
+++ src/components/common/PageContainer.tsx
@@ -1,6 +1,14 @@
-export default function PageContainer({children, title}: {children: React.ReactNode; title: React.ReactNode}) {
+export default function PageContainer({
+  children,
+  title,
+  className,
+}: {
+  children: React.ReactNode;
+  title: React.ReactNode;
+  className?: string;
+}) {
   return (
-    <div className="container mx-auto py-10 px-6">
+    <div className={`container mx-auto py-10 px-6 ${className}`}>
       <h2 className="mb-8 text-3xl">{title}</h2>
       {children}
     </div>
